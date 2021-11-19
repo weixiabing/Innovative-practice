f8eb848fee4e1374463a7d44ede1835bc92d2a6a
weixiabing <weixiabing@hotmail.com>
2021-11-19 08:52:35 +0800

    1

--- 3.py
+++ 3.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.holtwinters import SimpleExpSmoothing, ExponentialSmoothing
-pf=pd.read_csv('library/csv/pm/beijing_final1.csv')
+pf=pd.read_csv('library/csv/pm/shanghai.csv')
 decompose_result = seasonal_decompose(pf, model="multiplicative", period=8760)
 decompose_result.plot()
 plt.show()
