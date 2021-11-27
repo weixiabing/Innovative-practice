ebde6bbd9a9af18b5c16e1cbd1b0f457bc853803
Wei.Xiabing <weixiabing@hotmail.com>
2021-11-27 18:37:54 +0800

    1

--- 4预测.py
+++ 4预测.py
@@ -7,11 +7,11 @@ from statsmodels.tsa.holtwinters import SimpleExpSmoothing, ExponentialSmoothing
 pf=pd.read_csv('library/csv/pm/beijing_final1.csv')
 pf2=pd.read_csv('library/csv/pm/beijing_final2.csv')
 
-model = ExponentialSmoothing(pf["Value"], trend="add", seasonal="add", seasonal_periods=8760).fit()
-pred = model.forecast(24)
+model = ExponentialSmoothing(pf["Value"], trend="mul", seasonal="mul", seasonal_periods=8760).fit()
+pred = model.forecast(720)
 pred.to_csv("library/csv/pm/beijing_result.csv", encoding="utf_8_sig")
 plt.figure()
-plt.plot(range(24),pf2['Value'][0:23], color='r', label='Value')
+plt.plot(range(24),pf2['Value'][0:24], color='r', label='Value')
 plt.plot(range(24), pred , color='b', label='pred')
 
 plt.legend()
